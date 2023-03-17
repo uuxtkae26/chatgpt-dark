4101f8a6dfb1a67ebe0e2b7c2f866dee7cf8f9c6
CODEisArt <jaredlee0228@gmail.com>
2023-03-18 04:39:36 +0800

    update

--- src/system.ts
+++ src/system.ts
@@ -9,8 +9,10 @@ export const setting = {
 
 export const message = `- 由 [OpenAI API](https://platform.openai.com/docs/guides/chat) 提供支持。
 - 由 [@CODEisArt](https://github.com/CODEisArrrt) 基于 [chatgpt-vercel](https://github.com/ourongxing/chatgpt-vercel) 开发，查看 [源码](https://github.com/CODEisArrrt/chatgpt-node)，欢迎自部署。
-- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。点击输入框滚动到底部。`
+- [[Shift]] + [[Enter]] 换行，开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设，点击输入框滚动到底部。
+- 为了节省token消耗，默认不开启连续对话功能。
+- 当前没有给网站设置密码，请勿滥用。`
 
 export type Setting = typeof setting
 
-export const resetContinuousDialogue = false
+export const resetContinuousDialogue = true
